d305a4f658cd9566c8b43332efd0bdb8d75eaae9
Kamil Sobol <kasobol@microsoft.com>
2021-12-08 21:49:48 -0800

    container must exist now.

--- store-java-spring-boot-application-data-with-azure-blob-storage/final/src/main/java/com/azure/azurestorageblobdemo/service/BlobStorage.java
+++ store-java-spring-boot-application-data-with-azure-blob-storage/final/src/main/java/com/azure/azurestorageblobdemo/service/BlobStorage.java
@@ -7,7 +7,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import javax.annotation.PostConstruct;
+
 import com.azure.spring.core.resource.AzureStorageBlobProtocolResolver;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
@@ -18,10 +23,23 @@ import org.springframework.util.StreamUtils;
 @Service("BlobStorage")
 public class BlobStorage implements Storage {
 
+    private static final String CONTAINER_NAME = "file-uploader";
     private static final String RESOURCE_SEARCH_PATTERN_PREFIX = "azure-blob://file-uploader/";
 
     @Autowired
+    private BlobServiceClientBuilder blobServiceClientBuilder;
+
+    @Autowired
     private AzureStorageBlobProtocolResolver storageResourcePatternResolver;
+
+    @PostConstruct
+    private void init() {
+        BlobServiceClient blobServiceClient = blobServiceClientBuilder.buildClient();
+        BlobContainerClient blobContainerClient = blobServiceClient.getBlobContainerClient(CONTAINER_NAME);
+        if (!blobContainerClient.exists()) {
+            blobContainerClient.create();
+        }
+    }
 
     @Override
     public List<String> listFiles() throws IOException {
